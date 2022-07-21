93e96f1e503820e7bcb93155053d2ea51b2a52d9
vanessabou <boufassa.vanessa@gmail.com>
2022-07-21 15:58:17 +0200

    header & footer done

---
+++ src/components/Category.js
@@ -0,0 +1,13 @@
+import '../styles/Category.css'
+export default function Category (){
+    return(
+        <>
+            <ul className="list_category">
+            <li> Men</li>
+            <li> Women</li>
+            <li> Jewelery </li>
+            <li> Electronics </li>
+            </ul>
+      </>
+    )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import '../styles/Header.css';
 import logo from '../logo.png';
+import Category from './Category';
 import { FaUserAlt } from 'react-icons/fa';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { AiOutlineSearch } from 'react-icons/ai';
@@ -15,10 +16,11 @@ function Header (){
                 </a>
             </div>
             <div className='grid_item'>
-                
-                <div class="search" style={{marginRight:"30px;"}}>
+
+            <Category/>
+                <div class="search" >
                     <form action="/search" method="get" class="site-header__search small--hide" role="search">
-                        <input type="search" name="q" id="SiteNavSearch" placeholder="Recherche" aria-label="Recherche" class="site-header__search-input"/>
+                        <input className='search' type="search" name="q" id="SiteNavSearch" placeholder="Recherche" aria-label="Recherche" class="site-header__search-input"/>
                         <button type="submit" class="text-link site-header__link site-header__search-submit">
                             <AiOutlineSearch />
                         </button>
@@ -33,8 +35,7 @@ function Header (){
                     <a href="/cart" className="cart">
                     <AiOutlineShoppingCart/>
                     </a>
-                </div>
-                
+                </div> 
             </div>
 
         </div>
